b32551b6ccbef705abddf2ab620bfa1222b000fb
Illy85 <soaccentru@gmail.com>
2023-08-10 00:45:54 +0200

    Added new dynamic components

---
+++ src/presenter/header-presenter.js
@@ -0,0 +1,19 @@
+import {render, RenderPosition} from '../render.js';
+import HeaderView from '../view/header-view.js';
+
+const pageHeader = document.querySelector('.page-header');
+
+export default class HeaderPresenter {
+  TripBaseView = new HeaderView();
+
+
+  constructor({container}) {
+    this.container = container;
+  }
+
+  init() {
+    pageHeader.innerHTML = '';
+
+    render(this.TripBaseView, this.container, RenderPosition.AFTERBEGIN);
+  }
+}
