98cff52f22990c3152c4713c2611d80a0610416e
Illy85 <soaccentru@gmail.com>
2023-09-11 23:15:58 +0200

    Imported flatpickr library

--- src/util/common-tasks.js
+++ src/util/common-tasks.js
@@ -1,4 +1,34 @@
 import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+import { DurationFormats } from '../consts.js';
+
+dayjs.extend(duration);
+
+function calculateDuration(timeFrom, timeTo) {
+  return dayjs.duration(dayjs(timeTo).diff(timeFrom));
+}
+
+function getDuration(timeFrom, timeTo) {
+  const timeDuration = calculateDuration(timeFrom, timeTo);
+
+  const isDaysLong = Boolean(timeDuration.days());
+  const isHoursLong = Boolean(timeDuration.hours());
+
+  let format;
+
+  switch (true) {
+    case isDaysLong:
+      format = DurationFormats.DAYS;
+      break;
+    case isHoursLong:
+      format = DurationFormats.HOURS;
+      break;
+    default:
+      format = DurationFormats.MINUTES;
+  }
+
+  return timeDuration.format(format);
+}
 
 function humanizeDate(eventDate, dateFormat) {
   return eventDate ? dayjs(eventDate).format(dateFormat) : '';
@@ -29,4 +59,4 @@ function updatePoint(points, update) {
   return points.map((point) => point.id === update.id ? update : point);
 }
 
-export { getRandomArrayItem, getRandomNumber, humanizeDate, capitalize, updatePoint };
+export { getRandomArrayItem, getRandomNumber, humanizeDate, capitalize, updatePoint, getDuration };
